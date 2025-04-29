9d32205bb80b1340e0aeb8943843e465005382bb
woodpeckerland <hans.peter.specht@gmail.com>
2025-04-29 15:26:23 +0200

    MyGameUser Korrektur

--- app/src/main/java/de/bfw/mygameprojektspecht/MyGameUser.java
+++ app/src/main/java/de/bfw/mygameprojektspecht/MyGameUser.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class MyGameUser implements Serializable {
 
-    private static final long serialVersionUID = 1L;
     private String name;
     private String password;
     private int level;
